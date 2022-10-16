2c2148e2529a51f396451aa9063f423e62714016
Tania <banerjeetania2019@gmail.com>
2022-10-16 23:59:03 +0530

    new commit

---
+++ Beginner level/new.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+for(int i=0;i<n;i++){
+        if(arr[i]==search){
+            flag=true;
+            break;
+        }
+        else{
+            continue;
+        }
+    }
--- Codechef problems/SecondLargest.cpp
+++ Codechef problems/SecondLargest.cpp
@@ -9,7 +9,7 @@ int main() {
 	cin>>T;
 	for(int i=1;i<=T;i++){
         cin>>A>>B>>C;
-	    if(A>B){
+	    if(A>B && A>C){
 	        if(B>C){
 	            cout<<B<<endl;;
 	        }
@@ -17,7 +17,7 @@ int main() {
 	            cout<<C<<endl;
 	        }
 	    }
-	    else{
+	    else if(B>C && B>A){
 	        if(A>C){
 	            cout<<A<<endl;
 	        }
@@ -25,5 +25,13 @@ int main() {
 	            cout<<C<<endl;
 	        }
 	    }
+		else{
+			if(A>B){
+				cout<<A<<endl;
+			}
+			else{
+				cout<<B<<endl;
+			}
+		}
 	}
 }
